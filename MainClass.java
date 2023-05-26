0120d3af3081ca5d059a856a1bb49663a7b767c5
Fyyvictory <1174112315@qq.com>
2023-05-26 14:10:48 +0800

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -10,6 +10,7 @@ class MainClass {
 
     public static void main(String[] args) {
         Order order = new Order();
+        //方法一： order.getState(); blabla
         order.pay();
         order.deliver();
         order.sign();
